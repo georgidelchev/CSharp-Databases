57cf56a40770bc1d65f8e6767ac549cbc7ed2547
george <alienguymc@gmail.com>
2020-12-01 22:34:11 +0200

    add

---
+++ IntroductionToDatabases/P04_InsertRecordsInBothTables.sql
@@ -0,0 +1,10 @@
+-- Forth Task
+INSERT INTO Towns
+	VALUES (1, 'Sofia'),
+		   (2, 'Plovdiv'),
+		   (3, 'Varna');
+
+INSERT INTO Minions
+	VALUES(1, 'Kevin', 22, 1),
+		  (2, 'Kevin', 15, 3),
+		  (3, 'Steward', NULL, 2);
