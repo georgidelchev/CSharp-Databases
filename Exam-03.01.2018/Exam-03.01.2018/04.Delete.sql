ded68b8efa407f0c67d66175f42a09398b2395c5
george <alienguymc@gmail.com>
2020-12-02 10:59:40 +0200

    aaaaaa

--- Exam-03.01.2018/Exam-03.01.2018/04.Delete.sql
+++
@@ -1,2 +0,0 @@
-DELETE FROM Orders
-      WHERE ReturnDate IS NULL
--- Exam-03.01.2018/Exam-03.01.2018/06.YGeneration.sql
+++
@@ -1,7 +0,0 @@
-  SELECT c.FirstName,
-         c.LastName
-    FROM Clients c
-   WHERE DATEPART(YEAR, c.BirthDate) BETWEEN 1977 AND 1994
-ORDER BY c.FirstName,
-         c.LastName,
-         c.Id
