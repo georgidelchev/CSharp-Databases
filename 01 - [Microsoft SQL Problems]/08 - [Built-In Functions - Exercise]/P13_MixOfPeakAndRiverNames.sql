ce961e0d20423f8b0ee25bf6182a49567e33c088
Georgi Delchev <alienguymc@gmail.com>
2021-01-18 16:03:11 +0200

    Update P13_MixOfPeakAndRiverNames.sql

--- 01 - [Microsoft SQL Problems]/08 - [Built-In Functions - Exercise]/P13_MixOfPeakAndRiverNames.sql
+++ 01 - [Microsoft SQL Problems]/08 - [Built-In Functions - Exercise]/P13_MixOfPeakAndRiverNames.sql
@@ -1,4 +1,4 @@
-SELECT PeakName, RiverName, LOWER(CONCAT(PeakName, SUBSTRING(RiverName, 2, LEN(PeakName)))) AS Mix
+SELECT PeakName, RiverName, LOWER(CONCAT(PeakName, SUBSTRING(RiverName, 2, LEN(RiverName)-1))) AS Mix
     FROM Peaks,
          Rivers
     WHERE RIGHT(PeakName, 1) = LEFT(RiverName, 1)
