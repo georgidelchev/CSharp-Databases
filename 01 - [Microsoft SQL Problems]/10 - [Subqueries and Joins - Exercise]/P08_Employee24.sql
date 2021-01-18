c854e17afae59521a7782c0fb5976b31cdb24804
Georgi Delchev <alienguymc@gmail.com>
2021-01-18 16:07:19 +0200

    Update P08_Employee24.sql

--- 01 - [Microsoft SQL Problems]/10 - [Subqueries and Joins - Exercise]/P08_Employee24.sql
+++ 01 - [Microsoft SQL Problems]/10 - [Subqueries and Joins - Exercise]/P08_Employee24.sql
@@ -2,9 +2,11 @@ SELECT e.EmployeeID,
        e.FirstName,
        CASE
            WHEN YEAR(p.StartDate) >= 2005 THEN NULL
-           ELSE p.Name
+              ELSE p.Name
            END AS [ProjectName]
 FROM Employees AS e
          JOIN EmployeesProjects ep ON e.EmployeeID = ep.EmployeeID
          JOIN Projects p ON ep.ProjectID = p.ProjectID
 WHERE e.EmployeeID = 24
+
+
