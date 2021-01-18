405193de1bcb9b59f98a3d935adec4cb68b34548
Georgi Delchev <alienguymc@gmail.com>
2021-01-18 16:04:01 +0200

    Update P15_UserEmailProviders.sql

--- 01 - [Microsoft SQL Problems]/08 - [Built-In Functions - Exercise]/P15_UserEmailProviders.sql
+++ 01 - [Microsoft SQL Problems]/08 - [Built-In Functions - Exercise]/P15_UserEmailProviders.sql
@@ -1,3 +1,5 @@
-SELECT Username, SUBSTRING(Email, CHARINDEX('@', Email) + 1, LEN(Email)) AS [Email Provider]
+SELECT Username, 
+       SUBSTRING(Email, CHARINDEX('@', Email) + 1, LEN(Email)) AS [Email Provider]
     FROM Users
-    ORDER BY [Email Provider]
+    ORDER BY [Email Provider], 
+             Username
