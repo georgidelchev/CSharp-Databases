43ec51959bd79ded7e3849071bdd5b4909742a95
Georgi Delchev <alienguymc@gmail.com>
2021-01-18 16:06:21 +0200

    Update P14_EmployeesMinimumSalaries.sql

--- 01 - [Microsoft SQL Problems]/12 - [Indices And Data Aggregation - Exercise]/P14_EmployeesMinimumSalaries.sql
+++ 01 - [Microsoft SQL Problems]/12 - [Indices And Data Aggregation - Exercise]/P14_EmployeesMinimumSalaries.sql
@@ -1,4 +1,5 @@
-SELECT e.DepartmentID, FORMAT(MIN(Salary), 'F2') AS MinimumSalary
+SELECT e.DepartmentID, 
+       MIN(Salary) AS MinimumSalary
     FROM Employees AS e
     WHERE e.DepartmentID IN (2, 5, 7)
       AND e.HireDate > '01-01-2000'
