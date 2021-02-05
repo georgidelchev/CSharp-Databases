0d9b2d72018b7550a5bb5359c6c35e307ef8c0de
george <alienguymc@gmail.com>
2021-02-05 18:33:11 +0200

    add second solution to task

--- 01 - [Microsoft SQL Problems]/04 - [CRUD - Exercise]/P21_IncreaseSalaries.sql
+++ 01 - [Microsoft SQL Problems]/04 - [CRUD - Exercise]/P21_IncreaseSalaries.sql
@@ -1,6 +1,16 @@
+-- 1
 UPDATE Employees
 SET Salary *= 1.12
     WHERE DepartmentID IN (1, 2, 4, 11)
 
 SELECT Salary
     FROM Employees
+
+-- 2
+UPDATE Employees
+SET Salary = Salary * 1.12
+    WHERE DepartmentID IN (SELECT d.DepartmentID
+                               FROM Departments AS d
+                               WHERE d.Name IN ('Engineering', 'Tool Design', 'Marketing', 'Information Services'))
+
+SELECT Salary FROM Employees
