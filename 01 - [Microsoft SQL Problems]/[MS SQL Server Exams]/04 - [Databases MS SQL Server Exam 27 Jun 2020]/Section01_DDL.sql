519c1f4e020932b8a627990b8f388a8a83f8011c
Georgi Delchev <alienguymc@gmail.com>
2021-02-08 16:00:08 +0200

    Update Section01_DDL.sql

--- 01 - [Microsoft SQL Problems]/[MS SQL Server Exams]/04 - [Databases MS SQL Server Exam 27 Jun 2020]/Section01_DDL.sql
+++ 01 - [Microsoft SQL Problems]/[MS SQL Server Exams]/04 - [Databases MS SQL Server Exam 27 Jun 2020]/Section01_DDL.sql
@@ -1,12 +1,11 @@
 CREATE DATABASE WMS
-USE WMS
 
 CREATE TABLE Clients
 (
     ClientId  INT PRIMARY KEY IDENTITY,
     FirstName VARCHAR(50) NOT NULL,
     LastName  VARCHAR(50) NOT NULL,
-    Phone     CHAR(12)    NOT NULL
+    Phone     CHAR(12) CHECK (LEN(Phone) = 12)
 )
 
 CREATE TABLE Mechanics
@@ -26,40 +25,22 @@ CREATE TABLE Models
 CREATE TABLE Jobs
 (
     JobId      INT PRIMARY KEY IDENTITY,
-    ModelId    INT  NOT NULL,
-    Status     VARCHAR(11) DEFAULT 'Pending' CHECK (Status IN ('Pending', 'In Progress', 'Finished')),
-    ClientId   INT  NOT NULL,
-    MechanicId INT,
+    ModelId    INT  NOT NULL FOREIGN KEY REFERENCES Models (ModelId),
+    Status     VARCHAR(11) CHECK (Status IN ('Pending', 'In Progress', 'Finished')) DEFAULT 'Pending',
+    ClientId   INT  NOT NULL FOREIGN KEY REFERENCES Clients (ClientId),
+    MechanicId INT FOREIGN KEY REFERENCES Mechanics (MechanicId),
     IssueDate  DATE NOT NULL,
-    FinishDate DATE,
-
-    CONSTRAINT FK_Jobs_Models
-        FOREIGN KEY (ModelId)
-            REFERENCES Models (ModelId),
-
-    CONSTRAINT FK_Jobs_Clients
-        FOREIGN KEY (ClientId)
-            REFERENCES Clients (ClientId),
-
-    CONSTRAINT FK_Jobs_Mechanics
-        FOREIGN KEY (MechanicId)
-            REFERENCES Mechanics (MechanicId),
+    FinishDate DATE
 )
-
-
 
 CREATE TABLE Orders
 (
     OrderId   INT PRIMARY KEY IDENTITY,
-    JobId     INT NOT NULL,
+    JobId     INT NOT NULL FOREIGN KEY REFERENCES Jobs (JobId),
     IssueDate DATE,
-    Delivered BIT DEFAULT 0,
-
-    CONSTRAINT FK_Orders_Jobs
-        FOREIGN KEY (JobId)
-            REFERENCES Jobs (JobId)
+    Delivered BIT NOT NULL DEFAULT 0
 )
-  
+
 CREATE TABLE Vendors
 (
     VendorId INT PRIMARY KEY IDENTITY,
@@ -72,46 +53,26 @@ CREATE TABLE Parts
     SerialNumber VARCHAR(50) UNIQUE NOT NULL,
     Description  VARCHAR(255),
     Price        DECIMAL(18, 2) CHECK (Price > 0),
-    VendorId     INT                NOT NULL,
-    StockQty     INT                NOT NULL DEFAULT 0 CHECK (StockQty >= 0),
-
-    CONSTRAINT FK_Parts_Vendors
-        FOREIGN KEY (VendorId)
-            REFERENCES Vendors (VendorId),
+    VendorId     INT                NOT NULL FOREIGN KEY REFERENCES Vendors (VendorId),
+    StockQty     INT                NOT NULL DEFAULT 0 CHECK (StockQty >= 0)
 )
 
 CREATE TABLE OrderParts
 (
-    OrderId  INT                                NOT NULL,
-    PartId   INT                                NOT NULL,
-    Quantity INT DEFAULT 1 CHECK (Quantity > 0) NOT NULL,
+    OrderId  INT NOT NULL FOREIGN KEY REFERENCES Orders (OrderId),
+    PartId   INT NOT NULL FOREIGN KEY REFERENCES Parts (PartId),
+    Quantity INT NOT NULL DEFAULT 1 CHECK (Quantity > 0)
 
-    PRIMARY KEY (OrderId, PartId),
-
-    CONSTRAINT FK_OrderParts_Orders
-        FOREIGN KEY (OrderId)
-            REFERENCES Orders (OrderId),
-
-    CONSTRAINT FK_OrderParts_Parts
-        FOREIGN KEY (PartId)
-            REFERENCES Parts (PartId)
+        CONSTRAINT PK_OrderParts
+            PRIMARY KEY (OrderId, PartId)
 )
-  
+
 CREATE TABLE PartsNeeded
 (
-    JobId    INT NOT NULL,
-    PartId   INT NOT NULL,
+    JobId    INT NOT NULL FOREIGN KEY REFERENCES Jobs (JobId),
+    PartId   INT NOT NULL FOREIGN KEY REFERENCES Parts (PartId),
     Quantity INT NOT NULL DEFAULT 1 CHECK (Quantity > 0),
 
-    PRIMARY KEY (JobId, PartId),
-
-    CONSTRAINT FK_PartsNeeded_Jobs
-        FOREIGN KEY (JobId)
-            REFERENCES Jobs (JobId),
-
-    CONSTRAINT FK_PartsNeeded_Parts
-        FOREIGN KEY (PartId)
-            REFERENCES Parts (PartId)
+    CONSTRAINT PK_PartsNeeded
+        PRIMARY KEY (JobId, PartId)
 )
-
-
